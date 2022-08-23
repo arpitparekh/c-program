4d90248d0436ab5b004503c71c8794d669b2d7ae
arpitparekh <arpitparekh9@gmail.com>
2022-08-23 14:35:15 +0530

    string and patterns

---
+++ string.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main(){
+
+    
+        // String
+
+    // Strings are array of characters
+    // gropu of characters
+
+
+    //  "bhoomesh"
+
+
+    int arr[] = {1,2,3,4};
+
+    float arr1[] = {1.2,3.4,5.6};
+
+    char arr2[] = { 'a' , 'v' , 'c' , 'j' , 'k' };
+
+
+    /////////////////////////////////////////////////////
+
+
+    // char name[] = {'b','h','o','o','m','e','s','h'};   // String
+
+    char name1[] = "bhoomesh";
+
+    // for(int i=0;i<8;i++){
+
+    //     printf("%c",name[i]);
+
+    // }
+
+    printf("%s",name1);
+
+
+    return 0;
+
+
+}
+
---
+++ string_scanf.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+int main(){
+
+    printf("Enter String\n");
+
+    char arr[50];  // string
+
+    // scanf("%s",&arr); // not valid ?
+
+    // gets()  // puts()
+
+    gets(arr);  // get value from user // is a substitute for scanff(%s)
+
+    puts(arr);  // is a substitute for printf(%s)
+
+    // bhoomesh hello shy
+
+    // b => 1
+    // h => 4
+    // o => 3
+
+    // pointer
+
+    // function
+
+    // 
+
+
+
+}
